93db3c45c564e1e38b2b3967d342e0c6967efc39
Maansee <maansee313@gmail.com>
2017-07-01 21:41:57 +0530

    q5 using hive

---
+++ Hive/q5a.sql
@@ -0,0 +1,13 @@
+select year, job_title, count(job_title) as count_job from h1b_final where year='2011' group by job_title,year order by job_title desc limit 10;
+
+select year, job_title, count(job_title) as count_job from h1b_final where year='2012' group by job_title,year order by job_title desc limit 10;
+
+select year, job_title, count(job_title) as count_job from h1b_final where year='2013' group by job_title,year order by job_title desc limit 10;
+
+select year, job_title, count(job_title) as count_job from h1b_final where year='2014' group by job_title,year order by job_title desc limit 10;
+
+select year, job_title, count(job_title) as count_job from h1b_final where year='2015' group by job_title,year order by job_title desc limit 10;
+
+select year, job_title, count(job_title) as count_job from h1b_final where year='2016' group by job_title,year order by job_title desc limit 10;
+
+
