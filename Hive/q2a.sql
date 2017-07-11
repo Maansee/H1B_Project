a5fecf5708beca990bd94970a00cf213bee882c1
Maansee <maansee313@gmail.com>
2017-07-11 17:55:03 +0530

    updated files

--- Hive/q2a.sql
+++ Hive/q2a.sql
@@ -1,4 +1,4 @@
-select worksite, case_status,count(job_title) as count_job,count(case_status) as count_case from h1b_final where job_title like '%DATA ENGINEER%' and case_status='CERTIFIED' and year='2011' group by worksite,year,case_status order by count_job  desc limit 1;
+select year, worksite, case_status,count(job_title) as count_job,count(case_status) as count_case from h1b_final where job_title like '%DATA ENGINEER%' and case_status='CERTIFIED' and year='2011' group by worksite,year,case_status order by count_job  desc limit 1;
 
 select year, worksite, case_status,count(job_title) as count_job,count(case_status) as count_case from h1b_final where job_title like '%DATA ENGINEER%' and case_status='CERTIFIED' and year='2012' group by worksite,year,case_status order by count_job  desc limit 1;
 
